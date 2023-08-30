8bc9ab10af949f7c6e48c90252feeded352c8812
unknown <vnbconstructions6468@gmail.com>
2023-08-30 17:08:49 +0530

    first commit

---
+++ o-projects.html.php
@@ -0,0 +1,338 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+   <title>VNB Constructions</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/vnb%20logo%20white.jpg" rel="icon">
+  <link href="assets/img/vnb%20logo%20white.jpg" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,600;1,700&family=Roboto:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Work+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/main.css" rel="stylesheet">
+
+ 
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+ <header id="header" class="header d-flex align-items-center">
+    <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
+
+      <a href="index.html" class="logo d-flex align-items-center">
+        <!-- Uncomment the line below if you also wish to use an image logo -->
+        <img src="assets/img/vnb%20logo1.png" class="logo" alt=""> 
+        <!--<h1>VNB Constructions<span>.</span></h1> -->
+      </a>
+
+      <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
+      <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a href="index.html" class="active">Home</a></li>
+          <li><a href="about.html">About</a></li>
+          <li><a href="services.html">Services</a></li>
+          <li><a href="index.html#Mission">Mission</a></li>
+          <li><a href="index.html#Vision">Vision</a></li>
+          <li><a href="projects.html.php">Projects</a></li>
+          <li><a href="contact.html">Contact</a></li>
+        </ul>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+    
+  <main id="main">
+
+    <!-- ======= Breadcrumbs ======= -->
+     <div class="breadcrumbs d-flex align-items-center" style="background-image: url('assets/img/hero-carousel/C-pic_4.1.webp');">
+      <div class="container position-relative d-flex flex-column align-items-center" data-aos="fade">
+
+        <h2>Projects</h2>
+        <ol>
+          <li><a href="index.html">Home</a></li>
+          <li>Ongoing-Projects</li>
+        </ol>
+      </div>
+    </div><!-- End Breadcrumbs -->
+
+    <!-- ======= Our Projects Section ======= -->
+      <br>
+     <p class="text-center">Our pledge is to build everlasting relationship with our customers by exceeding their expectations and gaining their trust through exceptional perfomance at every stage of action. Our mission is to make you happy with good people and atmosphere around you.</p>
+      <br>
+      <div class="prj text-center">
+      <a href="projects.html.php"><button type="button" class="btn btn-primary btn-sm" style="background-color: grey""border-color: orange">Completed Project</button></a>
+         <a href=""> <button type="button" class="btn btn-primary btn-sm" style="background-color: orange""border-color: orange">Ongoing Project</button></a>
+      </div>
+      <section id="projects" class="projects" style="background-color: ghostwhite">
+      <div class="container" data-aos="fade-up">
+        <div class="portfolio-isotope" data-portfolio-filter="*" data-portfolio-layout="masonry" data-portfolio-sort="original-order">
+          <div class="row gy-4 portfolio-container" data-aos="fade-up" data-aos-delay="200">
+             
+<div class="col-lg-4 col-md-6 portfolio-item filter-remodeling">
+              <div class="portfolio-content h-100">
+                <img src="assets/img/ongoing-project/img1.webp" class="img-fluid" alt="">
+                <div class="portfolio-info">
+                  <h4>VNB Constructions</h4>
+                  <p></p>
+                  <a href="assets/img/ongoing-project/img1.webp" data-gallery="portfolio-gallery-remodeling" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                 
+                </div>
+              </div>
+            </div><!-- End Projects Item --> 
+              
+              <div class="col-lg-4 col-md-6 portfolio-item filter-remodeling">
+              <div class="portfolio-content h-100">
+                <img src="assets/img/ongoing-project/img2.webp" class="img-fluid" alt="">
+                <div class="portfolio-info">
+                  <h4>VNB Constructions</h4>
+                  <p></p>
+                  <a href="assets/img/ongoing-project/img2.webp" data-gallery="portfolio-gallery-remodeling" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                 
+                </div>
+              </div>
+            </div><!-- End Projects Item --> 
+              
+              <div class="col-lg-4 col-md-6 portfolio-item filter-remodeling">
+              <div class="portfolio-content h-100">
+                <img src="assets/img/ongoing-project/img3.webp" class="img-fluid" alt="">
+                <div class="portfolio-info">
+                  <h4>VNB Constructions</h4>
+                  <p></p>
+                  <a href="assets/img/ongoing-project/img3.webp" data-gallery="portfolio-gallery-remodeling" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                 
+                </div>
+              </div>
+            </div><!-- End Projects Item --> 
+              <div class="col-lg-4 col-md-6 portfolio-item filter-remodeling">
+              <div class="portfolio-content h-100">
+                <img src="assets/img/ongoing-project/img6.webp" class="img-fluid" alt="">
+                <div class="portfolio-info">
+                  <h4>VNB Constructions</h4>
+                  <p></p>
+                  <a href="assets/img/ongoing-project/img6.webp" data-gallery="portfolio-gallery-remodeling" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                 
+                </div>
+              </div>
+            </div><!-- End Projects Item --> 
+              <div class="col-lg-4 col-md-6 portfolio-item filter-remodeling">
+              <div class="portfolio-content h-100">
+                <img src="assets/img/ongoing-project/img5.webp" class="img-fluid" alt="">
+                <div class="portfolio-info">
+                  <h4>VNB Constructions</h4>
+                  <p></p>
+                  <a href="assets/img/ongoing-project/img5.webp" data-gallery="portfolio-gallery-remodeling" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                 
+                </div>
+              </div>
+            </div><!-- End Projects Item --> 
+               <div class="col-lg-4 col-md-6 portfolio-item filter-remodeling">
+              <div class="portfolio-content h-100">
+                <img src="assets/img/ongoing-project/img4.webp" class="img-fluid" alt="">
+                <div class="portfolio-info">
+                  <h4>VNB Constructions</h4>
+                  <p></p>
+                  <a href="assets/img/ongoing-project/img4.webp" data-gallery="portfolio-gallery-remodeling" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                 
+                </div>
+              </div>
+            </div><!-- End Projects Item --> 
+           
+               <div class="col-lg-4 col-md-6 portfolio-item filter-remodeling">
+              <div class="portfolio-content h-100">
+                <img src="assets/img/ongoing-project/img7.webp" class="img-fluid" alt="">
+                <div class="portfolio-info">
+                  <h4>VNB Constructions</h4>
+                  <p></p>
+                  <a href="assets/img/ongoing-project/img7.webp" data-gallery="portfolio-gallery-remodeling" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                 
+                </div>
+              </div>
+            </div><!-- End Projects Item --> 
+           
+               <div class="col-lg-4 col-md-6 portfolio-item filter-remodeling">
+              <div class="portfolio-content h-100">
+                <img src="assets/img/ongoing-project/img8.webp" class="img-fluid" alt="">
+                <div class="portfolio-info">
+                  <h4>VNB Constructions</h4>
+                  <p></p>
+                  <a href="assets/img/ongoing-project/img8.webp" data-gallery="portfolio-gallery-remodeling" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                 
+                </div>
+              </div>
+            </div><!-- End Projects Item --> 
+           
+               <div class="col-lg-4 col-md-6 portfolio-item filter-remodeling">
+              <div class="portfolio-content h-100">
+                <img src="assets/img/ongoing-project/img9.webp" class="img-fluid" alt="">
+                <div class="portfolio-info">
+                  <h4>VNB Constructions</h4>
+                  <p></p>
+                  <a href="assets/img/ongoing-project/img9.webp" data-gallery="portfolio-gallery-remodeling" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                 
+                </div>
+              </div>
+            </div><!-- End Projects Item --> 
+           
+          </div><!-- End Projects Container -->
+           
+        </div>
+           
+         
+      </div>
+         
+               
+            
+      </section>
+       
+    
+      
+
+      
+
+     
+      
+      
+      
+      
+    <section>
+       <div class="multiple-get-in-touch">
+						<div class="multiple-get-in-touch-inner">
+							<img src="assets/img/man.png" alt="VNB Construction">
+						            <div class="container">
+                <div class="row">
+								<div class="col-md-7 col-sm-12 multiple-get-in-touch-description">
+									<h3>GET YOUR DREAM HOME DONE?</h3>
+									<p style="color: black">We possess within us two minds so far i have written only of the conscious mind.</p>
+								</div>
+								<div class="col-md-5 col-sm-12">
+									<div class="row">
+										<div class="col-md-6 col-sm-8 multiple-get-in-touch-contact">
+											<div class="contact_detial">
+												<div class="phone_number">
+													<h5 style="color: black">call us now</h5>
+													<h2 class="phone_number_h2">9656296976</h2>
+												</div>
+											</div>
+										</div>
+                                        <br>
+                                        <br>
+										<div class="col-md-6 col-sm-4 my-auto ml-5 multiple-get-in-touch-button">
+											<a href="index.html#submit-form" style="color: black">GET IN TOUCH</a>
+										</div>
+                                        <div class="col-md-6 col-sm-4 multiple-get-in-touch-button1">
+											<a href="#" style="color: orange">GET IN TOUCH</a>
+										</div>
+									</div>								
+								</div>
+							</div>
+						</div>
+					</div>
+        </div>
+        </section>
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer" class="footer">
+
+    <div class="footer-content position-relative">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-4 col-md-6">
+            <div class="footer-info">
+              <h3 style="color: white">VNB Constructions</h3>
+              <p>
+                Mumthas tower <br>
+                Vaniyambalam<br>
+                Malappuram, 679339<br><br>
+                <strong>Phone:</strong> +91 9656 296 976<br>
+                <strong>Email:</strong> vnbconstructions6468@gmail.com<br>
+              </p>
+              <div class="social-links d-flex mt-3">
+                <a href="#" class="d-flex align-items-center justify-content-center"><i class="bi bi-twitter"></i></a>
+                <a href="#" class="d-flex align-items-center justify-content-center"><i class="bi bi-facebook"></i></a>
+                <a href="#" class="d-flex align-items-center justify-content-center"><i class="bi bi-instagram"></i></a>
+                <a href="#" class="d-flex align-items-center justify-content-center"><i class="bi bi-linkedin"></i></a>
+              </div>
+            </div>
+          </div><!-- End footer info column-->
+
+          <div class="col-lg-2 col-md-3 footer-links">
+            <h4>Quick Links</h4>
+            <ul>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">About</a></li>
+              <li><a href="#">Mission</a></li>
+              <li><a href="#">Vision</a></li>
+              <li><a href="#">Services</a></li>
+              <li><a href="projects.html.php">Projects</a></li>
+              <li><a href="#">Contacts</a></li>
+            </ul>
+          </div><!-- End footer links column-->
+
+          <div class="col-lg-2 col-md-3 footer-links">
+            <h4>Services</h4>
+            <ul>
+              <li><a href="#services">Residencial House</a></li>
+              <li><a href="#services">Apartments</a></li>
+              <li><a href="#services">Budget Homes</a></li>
+              <li><a href="#services">Commercial Buildings</a></li>
+              <li><a href="#services">Villas</a></li>
+            </ul>
+          </div><!-- End footer links column-->
+
+        </div>
+      </div>
+    </div>
+
+    <div class="footer-legal text-center position-relative">
+      <div class="container">
+        <div class="copyright" style="color: white">
+          &copy; Copyright <strong><span>VNB Constructions</span></strong>. All Rights Reserved
+        </div>
+      </div>
+    </div>
+
+  </footer>
+  <!-- End Footer -->
+
+  <a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <div id="preloader"></div>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
